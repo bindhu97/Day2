f0ffb016c096ad915c678b57d8e8996ee7d6f14c
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-15 11:36:12 +0530

    Sum of Commands

--- day2/src/main/java/com/bridgelabz/day2/SumOfCommands.java
+++ day2/src/main/java/com/bridgelabz/day2/SumOfCommands.java
@@ -6,7 +6,7 @@ public class SumOfCommands {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter any number: ");
+		System.out.println("Enter any numbers: ");
 		int s = input.nextInt();
 		int r = input.nextInt();
 		int sum = s + r;
