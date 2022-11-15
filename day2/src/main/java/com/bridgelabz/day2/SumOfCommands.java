f8bba9c4ea155aad1b7e692db9c4f63ffb68b1c4
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-15 11:18:57 +0530

    strings equal

---
+++ day2/src/main/java/com/bridgelabz/day2/SumOfCommands.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.day2;
+
+import java.util.Scanner;
+
+public class SumOfCommands {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int s = input.nextInt();
+		int r = input.nextInt();
+		int sum = s + r;
+		System.out.println("sum is " + sum);
+		input.close();
+
+	}
+
+}
