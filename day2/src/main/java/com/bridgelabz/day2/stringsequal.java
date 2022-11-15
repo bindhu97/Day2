1f11a2db67be503537a51ba695b1e94bd18c8819
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-15 07:22:31 +0530

    day2 commit

---
+++ day2/src/main/java/com/bridgelabz/day2/stringsequal.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day2;
+
+public class stringsequal {
+
+	public static void main(String[] args) {
+		String str1 = ("Hello");
+		String str2 = ("Hello");
+		System.out.println(str1.equals(str2));
+
+	}
+
+}
